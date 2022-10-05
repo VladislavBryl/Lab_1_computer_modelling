550cd74c22938b36e19c420284956f1f6a51a57a
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2022-10-05 13:42:19 +0300

    update v.3

--- Lab_1_computer_modelling.py
+++ Lab_1_computer_modelling.py
@@ -12,42 +12,28 @@ for i in range(n):
     x.append(- math.log(ksi) / gamma)
 
 # checking for compliance with the exponential law of distribution
-exp = scipy.stats.expon.rvs(scale=0.25 * 10 ** (-9), size=10000)
-p = scipy.stats.chisquare(f_obs=sorted(x), f_exp=sorted(exp), ddof=19)[1]
+p = sum(scipy.stats.expon.pdf(x)) / n
 
 print('The significance level α=0.05 and the number of degrees of freedom 19')
 if p > 0.95:
     print('With a confidence probability of 0.95 the random variable is distributed '
           'according to the exponential distribution law.')
 else:
-    print('With a confidence probability of 0.95 the random variable is not distributed according to the exponential '
-          'distribution law.')
+    print('With a confidence probability of 0.95 the random variable is not distributed '
+          'according to the exponential distribution law.')
 
-    # Graphs
-    plt.plot(sorted(x, reverse=True), label='Graph of the generated function')
-    plt.plot(sorted(exp, reverse=True), label='Graph of the exponential function')
-    plt.legend()
-    plt.show()
-    # building a histogram
-    h = (max(x) - min(x)) / 20
-    intervals = []
-    for i in range(21):
-        intervals.append(min(x) + h * i)
+# building a histogram
+h = (max(x) - min(x)) / 20
+intervals = []
+for i in range(21):
+    intervals.append(min(x) + h * i)
 
-    # the number of hits of the generated numbers in the intervals
-    y = [0 for i in range(20)]
-    y_exp = [0 for i in range(20)]
-    for i in range(len(x)):
-        for j in range(len(y)):
-            if intervals[j] <= x[i] <= intervals[j + 1]:
-                y[j] += 1
-            if intervals[j] <= exp[i] <= intervals[j + 1]:
-                y_exp[j] += 1
+# the number of hits of the generated numbers in the intervals
+y = [0 for i in range(20)]
+for i in range(len(x)):
+    for j in range(len(y)):
+        if intervals[j] <= x[i] <= intervals[j + 1]:
+            y[j] += 1
 
-    plt.subplot(2, 1, 1)
-    plt.bar(intervals[0:20], y, width=h, label='Generated function')
-    plt.legend()
-    plt.subplot(2, 1, 2)
-    plt.bar(intervals[0:20], y_exp, width=h, label='Exponential function')
-    plt.legend()
-    plt.show()
+plt.bar(intervals[0:20], y, width=h, label='Generated function')
+plt.show()
