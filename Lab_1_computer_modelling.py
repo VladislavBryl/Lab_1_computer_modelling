6380ab99910addad0ae507991b49061a38e60e56
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2022-10-05 13:32:22 +0300

    update v.2

--- Lab_1_computer_modelling.py
+++ Lab_1_computer_modelling.py
@@ -4,44 +4,50 @@ import math
 import scipy.stats
 
 n = 10000
-gamma = 100
+gamma = 4510129875
+print('gamma = ', gamma)
 x = []
 for i in range(n):
     ksi = random()
     x.append(- math.log(ksi) / gamma)
 
-h = (max(x) - min(x)) / 20
-intervals = []
-for i in range(21):
-    intervals.append(min(x) + h * i)
-
-# the number of hits of the generated numbers in the intervals
-y = [0 for i in range(20)]
-for i in range(len(x)):
-    for j in range(len(y)):
-        if intervals[j] <= x[i] <= intervals[j + 1]:
-            y[j] += 1
-
-        # checking for compliance with the exponential law of distribution
-arithmetic_mean = sum(x) / n
-l = 1 / arithmetic_mean
-p = []
-for i in range(20):
-    p.append(n * (math.exp(- l * intervals[i]) - math.exp(- l * intervals[i + 1])))
-chi2 = sum([(y[i] - p[i]) ** 2 / p[i] for i in range(len(y))])
-chi2_tabular = scipy.stats.chi2.ppf(q=0.99, df=19)
+# checking for compliance with the exponential law of distribution
+exp = scipy.stats.expon.rvs(scale=0.25 * 10 ** (-9), size=10000)
+p = scipy.stats.chisquare(f_obs=sorted(x), f_exp=sorted(exp), ddof=19)[1]
 
 print('The significance level α=0.05 and the number of degrees of freedom 19')
-print('The tabular value of the criterion: ', chi2_tabular)
-print('The calculated value of the criterion: ', chi2)
-
-if chi2 <= chi2_tabular:
-    print('With a confidence probability of 0.99 the random variable is distributed '
+if p > 0.95:
+    print('With a confidence probability of 0.95 the random variable is distributed '
           'according to the exponential distribution law.')
 else:
-    print('With a confidence probability of 0.99 the random variable is not distributed'
-    'according to the exponential distribution law.')
+    print('With a confidence probability of 0.95 the random variable is not distributed according to the exponential '
+          'distribution law.')
 
+    # Graphs
+    plt.plot(sorted(x, reverse=True), label='Graph of the generated function')
+    plt.plot(sorted(exp, reverse=True), label='Graph of the exponential function')
+    plt.legend()
+    plt.show()
     # building a histogram
-    plt.bar(intervals[0:20], y, width=h)
+    h = (max(x) - min(x)) / 20
+    intervals = []
+    for i in range(21):
+        intervals.append(min(x) + h * i)
+
+    # the number of hits of the generated numbers in the intervals
+    y = [0 for i in range(20)]
+    y_exp = [0 for i in range(20)]
+    for i in range(len(x)):
+        for j in range(len(y)):
+            if intervals[j] <= x[i] <= intervals[j + 1]:
+                y[j] += 1
+            if intervals[j] <= exp[i] <= intervals[j + 1]:
+                y_exp[j] += 1
+
+    plt.subplot(2, 1, 1)
+    plt.bar(intervals[0:20], y, width=h, label='Generated function')
+    plt.legend()
+    plt.subplot(2, 1, 2)
+    plt.bar(intervals[0:20], y_exp, width=h, label='Exponential function')
+    plt.legend()
     plt.show()
